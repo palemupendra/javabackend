1317e4d7329f573de9596c432f882d5558647221
palem upendra <palemupendra0209@gmail.com>
2025-06-27 21:13:25 +0530

    add the changes

---
+++ src/main/java/com/crmapp/entity/Customer.java
@@ -0,0 +1,16 @@
+package com.crmapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String company;
+
+    // Getters and setters
+}
